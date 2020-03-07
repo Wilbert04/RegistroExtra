f59d6a04bea6afb6ffb5c12c069e882f0aef2d5a
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-03-06 19:47:51 -0600

    Add project files.

---
+++ RegistroExtra/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroExtra.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroExtra.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = RegistroDB.db");
+        }
+    }
+}
